8720711e652d1b4a1a67ba16bbe71decc4585c16
m3ruem <109645090+m3ruem@users.noreply.github.com>
2024-02-21 12:39:01 +0800

    First Commit

---
+++ starter/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+import 'card1.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    final theme = FooderlichTheme.dark();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: const Home(),
+    );
+  }
+}
